0cdffb9a69e0fe1ad5d003a2280572edfd64b587
Bazil Khan <king.bazil.khan@gmail.com>
2022-03-28 08:55:35 +0000

    Odd checker

---
+++ odd.js
@@ -0,0 +1,10 @@
+function isOdd(number)  {
+  if(number % 2 === 1) {
+    return true;
+  } else  {
+    return false;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
